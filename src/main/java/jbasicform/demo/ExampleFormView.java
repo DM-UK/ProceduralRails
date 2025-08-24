c728577b93236e2d091912c3ff1a8ba71a1983bb
DM-UK <dan.mills.uk@gmail.com>
2025-08-24 10:02:09 +0100

    init

--- src/main/java/jbasicform/demo/ExampleFormView.java
+++
@@ -1,9 +0,0 @@
-package jbasicform.demo;
-
-public interface ExampleFormView {
-    String getProductName();
-    int getProductId();
-    String getCategory();
-    int getQuantity();
-    double getDiscount();
-}
